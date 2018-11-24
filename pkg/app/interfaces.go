c6715d1ce184a1d4310ced2e7c702b9e4e8fb047
Alexei  Stoliarov <Nick1021075@gmail.com>
2018-11-24 10:22:00 +0300

    generate mocks for IFetchService and IImageProcessingService

--- pkg/app/interfaces.go
+++ pkg/app/interfaces.go
@@ -1,9 +1,11 @@
 package app
 
+//go:generate mockgen -destination=../../tests/mocks/ifetchservice_mock.go -package=mocks jackal/pkg/app IFetchService
 type IFetchService interface {
 	GetBodyFromUrl(url string) ([]byte, error)
 }
 
+//go:generate mockgen -destination=../../tests/mocks/iimageprocessingservice_mock.go -package=mocks jackal/pkg/app IImageProcessingService
 type IImageProcessingService interface {
 	CropCentered(content []byte, width int, height int, cropType CropType) ([]byte, string, error)
 }
